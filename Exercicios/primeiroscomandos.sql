665d55dee4660c631cb2d4c4b4761f61da50f555
andreluiskunert <pesquisador036@gmail.com>
2025-04-25 16:40:23 -0300

    ArquivoTres

---
+++ Exercicios/primeiroscomandos.sql
@@ -0,0 +1,21 @@
+CREATE TABLE meupbd(
+ id INT(50),
+ nome VARCHAR(100),
+ email VARCHAR(150),
+ profissao VARCHAR(255),
+ site VARCHAR(150)
+);
+SELECT * FROM meupbd;
+/* INSERT INTO  meupbd
+VALUE( 01 ,"André Luis ", "alktecno@alktecno.com ","Desenvolvedor Full Stack PHP e Java", "alktecno.com" );*/
+ -- SHOW DATABASES;
+ -- SELECT * FROM teste;
+/* INSERT INTO  teste
+VALUE( 01 ,"André Luis ", "alktecno@gmail.com ","Desenvolvedor Full Stack" );
+ -- SELECT * FROM teste;
+INSERT INTO  teste
+VALUE( 04 ,"Jean Cardoso ", "jeancardosos@gmail.com ","arquiteto" );
+
+SELECT * FROM teste;
+DROP from  teste
+DROP TABLE teste;*/
